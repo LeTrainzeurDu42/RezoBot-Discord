41b5d369398d10f6a5d48d6b9264197b98169cd7
LeTrainzeurDu42 <41801566+LeTrainzeurDu42@users.noreply.github.com>
2018-07-28 13:21:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,7 +16,8 @@ bot.on("message", message => {
             message.channel.send("Bot en développement, merci de ne pas tenter de l'utiliser (tfaçon ça fera rien mdr)");
             break;
         case (prefix + "ping"):
-            message.channel.send("Mon ping est de **" + new Date().getTime() - message.createdTimestamp + "ms**.");
+            var ping = new Date().getTime() - message.createdTimestamp;
+            message.channel.send("Mon ping est de **" + ping + "ms**.");
             break;
     };
       
