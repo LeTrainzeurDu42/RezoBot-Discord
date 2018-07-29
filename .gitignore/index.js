96eea60e40f7253b9d5ebb960f10e6b95abf3058
LeTrainzeurDu42 <41801566+LeTrainzeurDu42@users.noreply.github.com>
2018-07-29 14:03:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("r!");
 
 bot.on('ready', function () {
-    bot.user.setActivity("BETA 0.2.4 - EN DEV");
+    bot.user.setActivity("BETA 0.2.5 - EN DEV");
     console.log("Connecté");
 });
 
@@ -38,20 +38,17 @@ bot.on("message", message => {
         message.channel.send("Bon, ca ira pour cette fois... Mon ping est de **" + ping + "ms**. Mais attention la prochaine fois hein :unamused:");
         break;
             
-    case (prefix + "test"):
-        message.channel.send("?role " + message.author.id + " A besoin d'aide");
-        break;
        
-    //case (prefix + "aide"):
-        //let membreAide = message.author
-        //let roleAide = message.guild.roles.find("id", "472802977661452292")
-        //if (message.author.roles.has(roleAide)) {
-            //message.reply("Tu as déjà demandé de l'aide ! Attends un peu !");
-        //} else {
-            //message.reply("Un @Staff a été prévenu, il sera à vous d'ici peu ! Rendez-vous dans le chat <#463112133849120771> :smile:");
-            //membreAide.addRole(roleAide.id);
-        //}
-       // break;
+    case (prefix + "aide"):
+        let membreAide = message.member
+        let roleAide = message.guild.roles.find("id", "472802977661452292")
+        if (membreAide.roles.has(roleAide)) {
+            message.reply("Tu as déjà demandé de l'aide ! Attends un peu !");
+        } else {
+            message.reply("Un @Staff a été prévenu, il sera à vous d'ici peu ! Rendez-vous dans le chat <#463112133849120771> :smile:");
+            membreAide.addRole(roleAide.id);
+        }
+        break;
     
     //case (prefix + "delrole aide"):
         //role.id("472802977661452292").members.removeRole("472802977661452292");
