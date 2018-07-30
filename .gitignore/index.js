74b708d8fb9672c33d83cfc2f5c1d9ff32cef805
LeTrainzeurDu42 <41801566+LeTrainzeurDu42@users.noreply.github.com>
2018-07-30 11:27:40 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("r!");
 
 bot.on('ready', function () {
-    bot.user.setActivity("BETA 0.2.9 - EN DEV");
+    bot.user.setActivity("BETA 0.3.0 - EN DEV");
     console.log("Connecté");
 });
 
@@ -13,48 +13,60 @@ bot.login(process.env.TOKEN);
 var aide = "Merci de ne pas utiliser le bot pour le moment.";
 
 
-bot.on("message", message => {
-    
-    
-    switch (message.content) {
-    case (prefix + "help"):
-        message.channel.send(aide);
-        break;
+bot.on("message", message =>
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        switch (command) {
+            case "ping":
+                var ping = new Date().getTime() - message.createdTimestamp;
+                message.channel.send("**PONG !** :ping_pong:");
+                message.channel.send("Mon ping est de **" + ping + "ms**.");
+                break;
+                
+            case "help":
+                message.channel.send(aide);
+                break;
             
-    case (prefix + "ping"):
-        var ping = new Date().getTime() - message.createdTimestamp;
-        message.channel.send("**PONG !** :ping_pong:");
-        message.channel.send("Mon ping est de **" + ping + "ms**.");
-        break;
+            case "site":
+                message.channel.send("Voici le site Internet du Rézo Quotidien ! https://rezoquotidien.wordpress.com/");
+                break;
+                
+            case "pong":
+                var ping = new Date().getTime() - message.createdTimestamp;
+                message.channel.send("**PON...** Keske");
+                message.channel.send("MAIS T'ES CON OU QUOI ??? LA COMMANDE C'EST `" + prefix + "ping` BORDEL !!");
+                message.channel.send("Bon, ca ira pour cette fois... Mon ping est de **" + ping + "ms**. Mais attention la prochaine fois hein :unamused:");
+                break;
+                
+            case "aide":
+                if (args[0] === undefined) {
+                    let membreAide = message.member;
+                    let roleAide = message.guild.roles.get("465803768307515392");
+                    if (membreAide.roles.has(roleAide.id)) {
+                        message.reply("tu as déjà demandé de l'aide ! Attends un peu !");
+                    } else {
+                        message.reply("un membre du <@&463112847468265512> a été prévenu, il sera à vous d'ici peu ! Rendez-vous dans le chat <#463112133849120771> :smile:");
+                        membreAide.addRole(roleAide);
+                    }
+                } else {
+                    let membreAide = message.mentions.members.first();
+                    if (membreAide === undefined) {
+                        message.reply("merci de mentionner un membre valide !");
+                    } else if (message.author.permission.has(MANAGE_ROLES) === false) {
+                        message.channel.send("Non.");
+                    } else {
+                        let roleAide = message.guild.roles.get("465803768307515392");
+                        if (membreAide.roles.has(roleAide.id)) {
+                            membreAide.removeRole(roleAide);
+                            message.channel.send("Succès !");
+                        } else {
+                            message.reply("ce membre n'a pas le rôle *A besoin d'aide*...");
+                        }
+                    }
+                }
             
-    case (prefix + "site"):
-        message.channel.send("Voici le site Internet du Rézo Quotidien ! https://rezoquotidien.wordpress.com/");
-        break;
-    
-    case (prefix + "pong"):
-        var ping = new Date().getTime() - message.createdTimestamp;
-        message.channel.send("**PON...** Keske");
-        message.channel.send("MAIS T'ES CON OU QUOI ??? LA COMMANDE C'EST `" + prefix + "ping` BORDEL !!");
-        message.channel.send("Bon, ca ira pour cette fois... Mon ping est de **" + ping + "ms**. Mais attention la prochaine fois hein :unamused:");
-        break;
-            
-       
-    case (prefix + "aide"):
-        let membreAide = message.member;
-        let roleAide = message.guild.roles.get("465803768307515392");
-        if (membreAide.roles.has(roleAide.id)) {
-            message.reply("tu as déjà demandé de l'aide ! Attends un peu !");
-        } else {
-            message.reply("un membre du <@&463112847468265512> a été prévenu, il sera à vous d'ici peu ! Rendez-vous dans le chat <#463112133849120771> :smile:");
-            membreAide.addRole(roleAide);
         }
-        break;
-    
-    //case (prefix + "delrole aide"):
-        //role.id("472802977661452292").members.removeRole("472802977661452292");
-        //message.channel.send("Succès !");
-       // break;
-    }
 });
 
 bot.on('guildMemberAdd', member => {
